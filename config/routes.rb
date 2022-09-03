8cb9b750829c4f2a9690059b5cb631cfbb4aae95
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-03 14:38:18 -0400

    commit tree 8856ec6ccb7bd22d3199840fdb81e7c3272cdc48

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show]
+  # resources :birds, only: [:index, :show, :create]
+
+  get '/birds', to: 'birds#index'
+  post 'birds', to: 'birds#create'
+
+  get '/birds/:id', to: 'birds#show'
+
+
+
 end
